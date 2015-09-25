722d44509102ad9d00ac8ef58bf3b17593dd533f
Sydothekido11 <sydnemelka@gmail.com>
2015-09-25 13:10:39 -0600

    Added more code to make the button and background change when the button is clicked.

--- app/src/main/java/ctec/pmstupidproject/StupidActivity.java
+++ app/src/main/java/ctec/pmstupidproject/StupidActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.RelativeLayout;
+import android.view.View;
+import android.graphics.Color;
 
 
 public class StupidActivity extends AppCompatActivity {
@@ -23,6 +25,8 @@ public class StupidActivity extends AppCompatActivity {
         colorChangeButton = (Button) findViewById(R.id.sillyButton);
         background = (RelativeLayout) findViewById(R.id.appBackground);
         sillyWords = (TextView) findViewById(R.id.sillyWords);
+
+        setupListeners();
 
 
     }
@@ -48,4 +52,40 @@ public class StupidActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void changeColors()
+    {
+        int redColor;
+        int greenColor;
+        int blueColor;
+
+        redColor = (int) (Math.random() * 256);
+        greenColor = (int) (Math.random() * 256);
+        blueColor = (int) (Math.random() * 256);
+
+        background.setBackgroundColor(Color.rgb(redColor, greenColor, blueColor));
+
+        redColor = (int) (Math.random() * 256);
+        greenColor = (int) (Math.random() * 256);
+        blueColor = (int) (Math.random() * 256);
+
+        colorChangeButton.setBackgroundColor(Color.rgb(redColor, greenColor, blueColor));
+
+    }
+
+    private void setupListeners()
+    {
+        colorChangeButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick (View buttonView)
+            {
+                //This is where you put code that happens when you click a button.
+                changeColors();
+            }
+
+
+        });
+    }
+
 }
